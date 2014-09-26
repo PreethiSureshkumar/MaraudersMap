798290cf2491e12b61b93166e6694a76e38b2aea
PreethiSureshkumar <psureshk@andrew.cmu.edu>
2014-09-26 18:45:16 -0400

    Initial Commit

---
+++ MaraudersMap/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MaraudersMap
+//
+//  Created by Preethi Sureshkumar on 9/26/14.
+//  Copyright (c) 2014 CMU. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
