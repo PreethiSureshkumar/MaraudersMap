12c8be1e32757da0e052a8fadae60ecd8873db9b
PreethiSureshkumar <psureshk@andrew.cmu.edu>
2014-09-27 12:46:57 -0400

    Adding Create and Join group controller

---
+++ MaraudersMap/CreateGroupViewController.swift
@@ -0,0 +1,27 @@
+//
+//  CreateGroupViewController.swift
+//  MaraudersMap
+//
+//  Created by Preethi Sureshkumar on 9/27/14.
+//  Copyright (c) 2014 CMU. All rights reserved.
+//
+
+import UIKit
+
+class CreateGroupViewController: UIViewController {
+   
+    @IBOutlet weak var expirationDateLabel: UILabel!
+    
+    @IBOutlet weak var expirationDatePicker: UIDatePicker!
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    }
---
+++ MaraudersMap/JoinGroupViewController.swift
@@ -0,0 +1,23 @@
+//
+//  JoinGroupViewController.swift
+//  MaraudersMap
+//
+//  Created by Preethi Sureshkumar on 9/27/14.
+//  Copyright (c) 2014 CMU. All rights reserved.
+//
+
+import UIKit
+
+class JoinGroupViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
