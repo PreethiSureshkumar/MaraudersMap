7092c6bfc633f55950a2019b77f83e1d9c194386
PreethiSureshkumar <psureshk@andrew.cmu.edu>
2014-09-27 10:52:07 -0400

    Adding 4 files

--- MaraudersMap/SignUpViewController.swift
+++ MaraudersMap/SignUpViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  SignUpViewController.swift
 //  MaraudersMap
 //
 //  Created by Preethi Sureshkumar on 9/26/14.
@@ -8,25 +8,28 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class SignUpViewController: UIViewController {
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-    @IBAction func verifyUsernamePassword(sender: AnyObject) {
+    
+    
+    @IBAction func verifyAndCreateAccount(sender: AnyObject) {
         
         let viewController:UIViewController = UIStoryboard(name: "Main", bundle:nil).instantiateViewControllerWithIdentifier("UserProfileViewController") as UIViewController
         // .instantiatViewControllerWithIdentifier() returns AnyObject! this must be downcast to utilize it
         
         self.presentViewController(viewController, animated: false, completion: nil)
+        
     }
-
+    @IBAction func cancelAccountCreation(sender: AnyObject) {
+        dismissViewControllerAnimated(true, completion: nil)
+    }
 }
-
