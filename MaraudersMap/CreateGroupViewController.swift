d3a2926af20d6062332e218db1cf1fd9be1e3932
PreethiSureshkumar <psureshk@andrew.cmu.edu>
2014-09-27 23:05:49 -0400

    Added AddressBook to Create Group Screen

--- MaraudersMap/CreateGroupViewController.swift
+++ MaraudersMap/CreateGroupViewController.swift
@@ -9,12 +9,13 @@
 import UIKit
 import AddressBook
 
-class CreateGroupViewController: UIViewController {
-   
+class CreateGroupViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    
     @IBOutlet weak var expirationDateLabel: UILabel!
     
     @IBOutlet weak var expirationDatePicker: UIDatePicker!
     
+    @IBOutlet weak var contactTableView: UITableView!
     lazy var addressBook:ABAddressBookRef = {
         var error: Unmanaged<CFError>?
         return ABAddressBookCreateWithOptions(nil, &error).takeRetainedValue() as ABAddressBookRef
@@ -22,11 +23,67 @@ class CreateGroupViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        switch ABAddressBookGetAuthorizationStatus(){
+        case .Authorized:
+            readFromAddressBook(addressBook)
+        case .Denied:
+            println("No access")
+        case .NotDetermined:
+            ABAddressBookRequestAccessWithCompletion(addressBook, {[weak self] (granted:Bool, error: CFError!) in
+                if granted{
+                    let strongSelf = self!
+                    println("Access is granted")
+                    strongSelf.readFromAddressBook(strongSelf.addressBook)
+                } else {
+                    println("No access")
+                }
+            })
+        case .Restricted:
+            println("Restricted")
+        default:
+            println("unhandled")
+        }
     }
     
+    var namesAndNumbers = [(String, String)]()
+    
+    func readFromAddressBook(addressBook:ABAddressBookRef){
+        let allPeople = ABAddressBookCopyArrayOfAllPeople(addressBook).takeRetainedValue() as NSArray
+        
+        namesAndNumbers = [(String, String)]()
+        for person: ABRecordRef in allPeople{
+            println(person)
+            var contactName: String = ABRecordCopyCompositeName(person).takeRetainedValue() as NSString
+            println ("contactName \(contactName)")
+            
+            let phoneNumbers: ABMultiValueRef = ABRecordCopyValue(person, kABPersonPhoneProperty).takeRetainedValue() as ABMultiValueRef
+            
+            var contactNo: String = ABMultiValueCopyValueAtIndex(phoneNumbers, ABMultiValueGetIndexForIdentifier(phoneNumbers, ABMultiValueGetIdentifierAtIndex(phoneNumbers, 0))).takeRetainedValue() as NSString
+            
+            println(contactNo)
+            
+            namesAndNumbers.append((contactName, contactNo))
+        }
+        
+    }
+
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
     
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return namesAndNumbers.count
     }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("GroupViewCell") as UITableViewCell
+        cell.textLabel!.text = namesAndNumbers[indexPath.row].0
+        cell.accessoryType = UITableViewCellAccessoryType.Checkmark
+        return cell
+    }
+    
+    func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        let number = namesAndNumbers[indexPath.row].1
+    }
+}
