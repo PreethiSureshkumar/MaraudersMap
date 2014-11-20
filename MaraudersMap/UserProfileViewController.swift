83f8cbf911407fa6ec4b09b5393cfe1b82ad93d0
PreethiSureshkumar <psureshk@andrew.cmu.edu>
2014-11-19 21:10:19 -0500

    List the user's group is done

--- MaraudersMap/UserProfileViewController.swift
+++ MaraudersMap/UserProfileViewController.swift
@@ -19,6 +19,7 @@ class UserProfileViewController: UIViewController, UITableViewDataSource, UITabl
     
     var groupName:String?
     var userName:String?
+    var itemsList = [String]()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -31,6 +32,48 @@ class UserProfileViewController: UIViewController, UITableViewDataSource, UITabl
         var tabBar:TabBarControllerMM = self.tabBarController as TabBarControllerMM
         self.userName = tabBar.userNameTab
         UserNameUserProfile.text = self.userName
+        
+        if let accountID = self.userName {
+                
+                let urlPath = "http://ec2-54-86-76-107.compute-1.amazonaws.com:8080/alpha/list-group?account=\(accountID)"
+                
+                println(urlPath)
+                
+                let url: NSURL! = NSURL(string: urlPath)
+                
+                let session = NSURLSession.sharedSession()
+                
+                let task = session.dataTaskWithURL(url, completionHandler: {data, response, error -> Void in
+                    println("Task completed")
+                    if(error != nil) {
+                        // If there is an error in the web request, print it to the console
+                        println(error.localizedDescription)
+                    } else {
+                        println(data)
+                    }
+                    var err: NSError?
+                    var jsonResult = NSJSONSerialization.JSONObjectWithData(data, options: NSJSONReadingOptions.MutableContainers, error: &err) as NSDictionary
+                    if(err != nil) {
+                        // If there is an error parsing JSON, print it to the console
+                    }
+                    let results: String = jsonResult["status"] as String
+                    println(results)
+                    if results == "OK" {
+                        if let groupArr = jsonResult["groupid"] as? NSArray{
+                            for (index, value) in enumerate(groupArr)
+                            {
+                                var item : String = groupArr[index] as String
+                                self.itemsList+=[item]
+                            }
+                            dispatch_async(dispatch_get_main_queue(), {
+                                self.groupTableView.reloadData()
+                            })
+                        }
+                    }
+                })
+                task.resume()
+        }
+
     }
     
     override func viewDidAppear(animated: Bool) {
@@ -42,7 +85,7 @@ class UserProfileViewController: UIViewController, UITableViewDataSource, UITabl
     override func viewWillLayoutSubviews() {
         groupTableView.sectionHeaderHeight = 0.0
         groupTableView.sectionFooterHeight = 0.0
-        groupTableViewHeightConstraint.constant = groupTableView.contentSize.height
+        groupTableView.reloadData()
     }
     
     override func didReceiveMemoryWarning() {
@@ -50,21 +93,19 @@ class UserProfileViewController: UIViewController, UITableViewDataSource, UITabl
         // Dispose of any resources that can be recreated.
     }
     
-    var itemsList = ["Group 1", "Group 2", "Group 3"]
-    
     func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return itemsList.count
+        return self.itemsList.count
     }
     
     func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCellWithIdentifier("GroupViewCell") as UITableViewCell
-        cell.textLabel.text = itemsList[indexPath.row]
+        cell.textLabel.text = self.itemsList[indexPath.row]
         cell.accessoryType = UITableViewCellAccessoryType.DisclosureIndicator
         return cell
     }
     
     func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
-        groupName = itemsList[indexPath.row]
+        groupName = self.itemsList[indexPath.row]
         self.performSegueWithIdentifier("LoadMapSegue", sender: self)
         
     }
